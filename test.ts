65ef5c6b6c7f86baea2c93d65d82763993825c60
TalhaYaseen075 <muhammadty785@gmail.com>
2024-06-23 10:15:34 +0500

    message

---
+++ test.ts
@@ -0,0 +1,10 @@
+let nums: number [] = [1, 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10];
+
+nums.forEach((element: number , index: number , refArray: number []): void => {
+    console.log("element" , element);
+    console.log("index" , index);
+    console.log("refArray" , refArray);
+});  
+
+
+let user={}
